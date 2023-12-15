cb0fd2529e0442f133e9092829eec299a8230b9b
Reg1str <ilya.n1konov@inbox.ru>
2023-12-15 13:56:16 +0300

    loading time fix

--- src/scripts/loadingTime.js
+++ src/scripts/loadingTime.js
@@ -1,10 +1,8 @@
-let before_loadtime = new Date().getTime();
 window.onload = Pageloadtime;
 
-    function Pageloadtime() {
-        let aftr_loadtime = new Date().getTime();
-        let pgloadtime = (aftr_loadtime - before_loadtime)
-        document.getElementById("Timer").innerHTML = 'Page load time is ' + pgloadtime.toString() + ' ms';
+    function Pageloadtime(){
+        let start = performance.timing.navigationStart
+        let currentTime = Date.now()
+        let loadTime = currentTime - start
+        document.getElementById("Timer").innerHTML = 'Page load time is ' + loadTime.toString() + ' ms';
     }
-
-// document.addEventListener('DOMContentLoaded', onPageLoad);
